bf8a601318a0b33f4d25e978d3c97683df49e446
temitayo <tytunji29@gmail.com>
2023-04-03 13:00:41 +0100

    done

---
+++ SSP/Controllers/MonthlyRemitance/Employee.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SSP.Controllers.MonthlyRemitance
+{
+    public class Employee : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
