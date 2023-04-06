3dc1dc27afc352ffaa3e0ec0abcdf65b9253dac2
jamesiukalu <jkalu@nownow.com>
2023-04-06 16:19:19 +0100

    ok

---
+++ SSP/Controllers/FormH1(AnnualReturn)/Objections.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SSP.Controllers.FormH1_AnnualReturn_
+{
+    public class Objections : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
